57b0e6d96264819176bf7150254b856585a080d6
madisonparker <christaandmadison@gmail.com>
2018-07-24 17:54:42 +0000

    automatically backed up by learn

--- practice.rb
+++ practice.rb
@@ -1,2 +1,11 @@
 puts "What time is it?"
 time = get.chomp 
+
+If time < 5
+puts "dont talk to me"
+
+elsif time > 5 && < 10 
+puts "good morning"
+
+elsif time > 10 && time < 16
+puts "good afternoon"
